6e70916b52839d7d7beaa1f98b61be3cc79a0d38
HoangTran223 <tranvuonghoang204@gmail.com>
2025-04-11 20:53:27 +0700

    first update

---
+++ attention.py
@@ -0,0 +1,56 @@
+﻿import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math 
+
+class SelfAttention(nn.Module):
+    def __init__(self, n_heads: int, d_embed: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+
+        self.in_proj = nn.Linear(d_embed, d_embed * 3, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    # mask: avoid particular tokens
+    def forward(self, x: torch.Tensor, casual_mask=False):
+        # x: (Batch_size, seq_len, dim)
+
+        input_shape = x.shape  
+        batch_size, sequence_leangth, d_embed = input_shape 
+
+        intermin_shape = (batch_size, sequence_leangth, self.n_heads, self.d_head)
+
+        # (Batch_size, seq_len, dim) -> (Batch_size, seq_len, dim * 3) -> 3 tensors of shape (Batch_size, seq_len, dim)
+        q, k, v = self.in_proj(x).chunk(3, dim=-1)
+
+        # (batch_size, seq_len, dim) -> (Batch_size, seq_len, n_heads, dim // n_heads) -> (Batch_size, H, seq_len, dim // H)
+        q = q.view(intermin_shape).transpose(1, 2)
+        k = k.view(intermin_shape).transpose(1, 2)
+        v = v.view(intermin_shape).transpose(1, 2)
+
+        # (Batch_size, H, seq_len, seq_len)
+        weight = q @ k.transpose(-1, -2)
+
+        if casual_mask:
+            # mask where the upper triangle
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
+            weight.masked_fill_(mask, float('-inf'))
+        
+        weight /= math.sqrt(self.d_head)
+        weight = F.softmax(weight, dim=-1)
+
+        # (Batch_size, H, seq_len, dim / H)
+        output = weight @ v
+
+        # (Batch_size, seq_len, H, dim / H)
+        output = output.transpose(1, 2)
+
+        output = output.reshape(input_shape)
+        output = self.out_proj(output)
+
+        # (Batch_size, seq_len, dim)
+        return output
+
+
---
+++ sd/clip.py
@@ -0,0 +1,72 @@
+﻿import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from attention import SelfAttention
+
+class CLIPEmbedding(nn.Module):
+    def __init__(self, n_vocab: int, n_embd: int, n_tokens:int):
+        super().__init__()
+        self.token_embedding = nn.Embedding(n_vocab, n_embd)
+        self.position_embedding = nn.Parameter(torch.zeros(n_tokens, n_embed))
+    
+    def forward(self, tokens):
+        # (batch_size, seq_len) -> (batch_size, seq_len, dim)
+        x = self.token_embedding(tokens)
+
+        x += self.position_embedding
+        return x 
+
+class CLIPLayer(nn.Module):
+    def __init__(self, n_head: int, n_embd: int):
+        super().__init__()
+        self.layernorm_1 = nn.LayerNorm(n_embd)
+        self.attention  = SelfAttention(n_head, n_embd)
+        self.layernorm_2 = nn.LayerNorm(n_embd)
+        self.linear_1 = nn.Linear(n_embd, n_embd * 4)
+        self.linear_2 = nn.Linear(n_embd * 4, n_embd)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        
+        # (batch_size, seq_len, dim) 
+        residue = x
+
+        ## self-attention
+        x = self.layernorm_1(x)
+        x = self.attention(x)
+        x += residue
+
+        ## FFN 
+        x = self.layernorm_2(x)
+        x = self.linear_1(x)
+
+        x = x * torch.sigmoid(x * 1.702) # Quick GeLU activation
+        x = self.linear_2(x)
+        x += residue
+        return x 
+
+
+class CLIP(nn.Module):
+    def __init__(self):
+        self.embedding = CLIPEmbedding(49408, 768, 77)
+        self.layers = nn.Module(
+            # 12: number of head of the attention, 768 is the embedding size
+            [CLIPLayer(12, 768)  for i in range(12)]
+        )
+
+        self.layernorm = nn.LayerNorm(768)
+
+    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
+        tokens = tokens.type(torch.long)
+
+        #v(batch_size, seq_len) -> (batch_size, seq_len, dim)
+        state = self.embedding(tokens)
+
+        for layer in self.layers:
+            state = layer(state)
+        
+        # (batch_size, seq_len, dim)
+        output = self.layernorm(state)
+
+
+
+
