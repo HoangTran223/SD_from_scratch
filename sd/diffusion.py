7eb6cf896cd1abc110edfd43f332d6daf7eb0557
HoangTran223 <tranvuonghoang204@gmail.com>
2025-04-11 23:23:09 +0700

    Second update

---
+++ sd/diffusion.py
@@ -0,0 +1,237 @@
+﻿import torch 
+from torch import nn
+from torch.nn import functional as F   
+from attention import SelfAttention, CrossAttention
+
+
+class TimeEmbedding(nn.Module):
+    def __init__(self, n_embd: int):
+        super().__init__()
+        self.linear_1 = nn.Linear(n_embd, n_embd * 4)
+        self.linear_2 = nn.Linear(4 * n_embd, 4 * n_embd)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (1, 320)
+        x = self.linear_1(x)
+        x = F.silu(x)
+        x = self.linear_2(x)
+
+        # (1, 1280) -> (1, 320)
+        return x
+
+class UNET_ResidualBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, n_time = 1280):
+        super().__init__()
+        self.groupnorm_feature = nn.GroupNorm(32, in_channels)
+        self.conv_feature = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.linear_time = nn.Linear(n_time, out_channels) 
+        
+        self.groupnorm_merged = nn.GroupNorm(32, out_channels) 
+        self.conv_merged = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0) 
+        
+    def forward(sel, feature, time):
+        # feature: (batch_size, in_channels, height, width)
+        # time: (1, 1280)
+        residue = feature
+        feature = self.groupnorm_feature(feature)
+        feature = F.silu(feature)
+        feature = self.conv_feature(feature)
+
+        time = F.silu(time)
+        time = self.linear_time(time)
+        
+        merged = feature + time.unsqueeze(-1).unsqueeze(-1)
+        merged = self.groupnorm_merged(merged)
+        merged = F.silu(merged)
+        merged = self.conv_merged(merged)
+
+        return merged + self.residual_layer(residue)
+
+
+class UNET_AttentionBlock(nn.Module):
+    def __init__(self, n_head: int, n_embd: int, d_context=768):
+        super().__init__()
+        channels = n_head * n_embd 
+        self.groupnorm = nn.GroupNorm(32, channels, eps=1e-6)
+        self.conv_input = nn.Conv2d(channels, channels, kernel_size=3, padding=0)
+
+        self.layernorm_1 = nn.LayerNorm(channels)
+        self.attention_1 = SelfAttention(n_head, channels, in_proj_bias=False)
+        self.layernorm_2 = nn.LayerNorm(channels)
+        self.attention_2 = CrossAttention(n_head, channels, d_context, in_proj_bias=False)
+        self.layernorm_3 = nn.LayerNorm(channels)
+        self.linear_geglu_1 = nn.Linear(channels, 4 * channels * 2)
+        self.linear_geglu_2 = nn.Linear(4 * channels * 2, channels)
+
+        self.conv_output = nn.Conv2d(channels, channels, kernel_size=3, padding=0)
+    
+    def forward(self, x, context):
+        # (batch_size, features, height, width)
+        # context: (batch_size, seq_len, dim)
+
+        residue_long = x 
+        x = self.groupnorm(x)
+        x = self.conv_input
+
+        n, c, h, w = x.shape
+
+        # (batch_size, features, height * width)
+        x = x.view(n, c, h * w) 
+
+        # (batch_size, height * width. features)
+        x = x.transpose(-1, -2)
+
+        # Normalization + self attention with skip connection
+        residue_short = x 
+        x = self.layernorm_1(x)
+        self.attention_1(x)
+        x += residue_short
+        residue_short = x 
+
+        # Normalization + cross attention with skip connection
+        x = self.layernorm_2(x)
+        # Cross Attention
+        self.attention_2(x, context)
+
+        x += residue_short 
+        residue_short = x
+
+        # Normalization + FF with GeGLU and skip connection
+
+        x = self.layernorm_3(x)
+        x, gate = self.linear_geglu_1(x).chunk(2, dim=-1)
+        x = x * F.gelu(gate)
+
+        x = self.linear_geglu_2(x)
+        x += residue_short 
+        x = x.transpose(-1, -2)
+
+        x = x.view(n, c, h, w)
+        return self.conv_output(x) + residue_long
+
+
+
+
+
+class  SwithSequential(nn.Sequential):
+    def forward(self, x: torch.Tensor, context: torch.Tensor, time: torch.Tensor) -> torch.Tensor:
+        for layer in self:
+            if isinstance(layer, UNET_AttetionBlock):
+                x = layer(x, context)
+            elif isinstance(layer, ResidualBlock):
+                x = layer(x, time)
+            else:
+                x = layer(x)
+        return x
+
+class UNET(nn.Module):
+    def __init__(self):
+        super_().__init__()
+        self.encoder = nn.Module([
+            # (batch_size, 4, Height / 8, Width / 8)
+            SwithSequential(nn.Conv2d(4, 320, kernel_size=3, padding=1)),
+
+            SwithSequential(UNET_ResidualBlock(320, 320), UNET_AttentionBlock(8, 40)),
+            SwithSequential(UNET_ResidualBlock(320, 320), UNET_AttentionBlock(8, 40)),
+
+            # (batch_size, 320, Height / 8, Width / 8) -> (batch_size, 320, Height / 16, Width / 16)
+            SwithSequential(nn.Conv2d(320, 320, kernel_size=3, stride=2, padding=1)),
+
+            SwithSequential(UNET_ResidualBlock(320, 640), UNET_AttentionBlock(8, 80)),
+            SwithSequential(UNET_ResidualBlock(640, 640), UNET_AttentionBlock(8, 80)),
+
+            # (batch_size, 640, Height / 16, Width / 16) -> (batch_size, 640, Height / 32, Width / 32)
+            SwithSequential(nn.Conv2d(640, 640, kernel_size=3, stride=2, padding=1)),
+
+            SwithSequential(UNET_ResidualBlock(640, 1280), UNET_AttentionBlock(8, 160)),
+            SwithSequential(UNET_ResidualBlock(640, 1280), UNET_AttentionBlock(8, 160)),
+
+            # (batch_size, 1280, Height / 32, Width / 32) -> (batch_size, 1290, Height / 64, Width / 64)
+            SwithSequential(nn.Conv2d(640, 640, kernel_size=3, stride=2, padding=1)),
+
+            SwithSequential(UNET_ResidualBlock(640, 1280), UNET_AttentionBlock(8, 160)),
+            SwithSequential(UNET_ResidualBlock(640, 1280), UNET_AttentionBlock(8, 160)),
+        ])
+
+    self.bottleneck = SwithSequential(
+        UNET_ResidualBlock(1280, 1280),
+        UNET_AttentionBlock(8, 160),
+        UNET_ResidualBlock(1280, 1280),
+    )
+
+    self.decoders = nn.ModuleList([
+        # (batch_size, 2560, Height / 64, Width / 64) -> (batch_size, 1280, Height / 64, Width / 64)
+        SwithSequential(UNET_ResidualBlock(2560, 1280)),
+        SwithSequential(UNET_ResidualBlock(2560, 1280)),
+        SwithSequential(UNET_ResidualBlock(2560, 1280), Upsample(1280)),
+
+        SwithSequential(UNET_ResidualBlock(2560, 1280), UNET_AttentionBlock(8, 160)),
+        SwithSequential(UNET_ResidualBlock(2560, 1280), UNET_AttentionBlock(8, 160)),
+        SwithSequential(UNET_ResidualBlock(1920, 1280), UNET_AttentionBlock(8, 160), Upsample(1280)),
+
+        SwithSequential(UNET_ResidualBlock(1920, 640), UNET_AttentionBlock(8, 80)),
+        SwithSequential(UNET_ResidualBlock(1280, 640), UNET_AttentionBlock(8, 80)),
+        SwithSequential(UNET_ResidualBlock(960, 640), UNET_AttentionBlock(8, 80), upSample(640)),
+
+        SwithSequential(UNET_ResidualBlock(960, 320), UNET_AttentionBlock(8, 40)),
+
+        # Do skip-connection -> concat feature encode + decoder, giu nguyen 80 tu encoder
+        SwithSequential(UNET_ResidualBlock(640, 320), UNET_AttentionBlock(8, 80)),
+        SwithSequential(UNET_ResidualBlock(1280, 640), UNET_AttentionBlock(8, 40)),
+    ])
+
+
+class UNET_OutputLayer(nn.Module):
+    def _init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, in_channels)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+    
+    def forward(self, x):
+        # (batch_size, 320, height / 8, width / 8) 
+        x = self.groupnorm(x)
+
+        x = F.silu(x)
+
+        # (batch_size, 4, height / 8, width / 8)
+        x = self.conv(x)
+        return x 
+
+
+
+class Upsample(nn.Module):
+    def __init__(sekf, channels: int):
+        super().__init__()
+        self.conv = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+    
+    def forward(self, x):
+        #(batch_size, features, height, width) -> (batch_size, features, height * 2, width * 2)
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        return self.conv(x)
+
+class Diffusion(nn.Module):
+    def __init_(self):
+        self.time_embedding = TimeEmbedding(320, 4)
+        self.unet = UNET()
+        self.final = UNET_OutputLayer(320, 4)
+    
+    def forward(self, latent: torch.Tensor, context: torch.Tensor, time: torch.Tensor):
+        # latent: (batch_size, 4, h / 8, w / 8)
+        # context: (batch_size, seq_len, dim)
+        # time: (1, 320)
+
+        # (1, 1280)
+        time = self.time_embedding(time)
+
+        # (batch_size, 4, h / 8, w / 8) -> (batch_size, 320, h / 8, w / 8)
+        output = self.unet(latent, context, time)
+
+        # (batch_size, 320, h / 8, w / 8) -> (batch_size, 4, h / 8, w / 8)
+        output = self.final(output)
+        return output
+    
---
+++ sd/pipeline.py
@@ -0,0 +1,173 @@
+﻿import torch
+import numpy as np
+from tqdm import tqdm
+from ddpm import DDPMSampler
+
+WIDTH = 512
+HEIGHT = 512
+LATENTS_WIDTH = WIDTH // 8
+LATENTS_HEIGHT = HEIGHT // 8
+
+def generate(
+    prompt,
+    uncond_prompt=None,
+    input_image=None,
+    strength=0.8,
+    do_cfg=True,
+    cfg_scale=7.5,
+    sampler_name="ddpm",
+    n_inference_steps=50,
+    models={},
+    seed=None,
+    device=None,
+    idle_device=None,
+    tokenizer=None,
+):
+
+    with torch.no_grad():
+        if idle_device:
+            to_idle = lambda x: x.to(idle_device)
+        else:
+            to_idle = lambda x: x
+
+        # Initialize random number generator
+        generator = torch.Generator(device=device)
+        if seed is None:
+            generator.seed()
+        else:
+            generator.manual_seed(seed)
+
+        clip = models["clip"]
+        clip.to(device)
+        
+        if do_cfg:
+            # list of length seq_len=77
+            cond_tokens = tokenizer.batch_encode_plus([prompt], padding="max_length", max_length=77).input_ids
+
+            # (batch_size, seq_len)
+            cond_tokens = torch.tensor(cond_tokens, dtype=torch.long, device=device)
+
+            # (batch_size, seq_len, dim)
+            cond_context = clip(cond_tokens)
+
+            # list of length seq_len=77
+            uncond_tokens = tokenizer.batch_encode_plus([uncond_prompt], padding="max_length", max_length=77).input_ids
+
+            # (batch_size, seq_len)
+            uncond_tokens = torch.tensor(uncond_tokens, dtype=torch.long, device=device)
+
+            # (batch_size, seq_len, dim)
+            uncond_context = clip(uncond_tokens)
+
+            # (2 * batch_size, seq_len, dim)
+            context = torch.cat([cond_context, uncond_context])
+
+        else:
+            # list of length seq_len=77
+            tokens = tokenizer.batch_encode_plus([prompt], padding="max_length", max_length=77).input_ids
+
+            # (batch_size, seq_len)
+            tokens = torch.tensor(tokens, dtype=torch.long, device=device)
+
+            #(batch_size, seq_len, dim)
+            context = clip(tokens)
+            
+        to_idle(clip)
+
+        if sampler_name == "ddpm":
+            sampler = DDPMSampler(generator)
+            sampler.set_inference_timesteps(n_inference_steps)
+        else:
+            raise ValueError("Unknown")
+
+        latents_shape = (1, 4, LATENTS_HEIGHT, LATENTS_WIDTH)
+
+        if input_image:
+            encoder = models["encoder"]
+            encoder.to(device)
+
+            input_image_tensor = input_image.resize((WIDTH, HEIGHT))
+            # (Height, Width, Channel)
+            input_image_tensor = np.array(input_image_tensor)
+
+            # (Height, Width, Channel)
+            input_image_tensor = torch.tensor(input_image_tensor, dtype=torch.float32, device=device)
+
+            # (Height, Width, Channel)
+            input_image_tensor = rescale(input_image_tensor, (0, 255), (-1, 1))
+
+            # (batch_size, Height, Width, Channel)
+            input_image_tensor = input_image_tensor.unsqueeze(0)
+
+            # (batch_size, Channel, Height, Width)
+            input_image_tensor = input_image_tensor.permute(0, 3, 1, 2)
+
+            # (batch_size, 4, Latents_Height, Latents_Width)
+            encoder_noise = torch.randn(latents_shape, generator=generator, device=device)
+            latents = encoder(input_image_tensor, encoder_noise)
+
+            # Add noise to the latents
+            sampler.set_strength(strength=strength)
+            latents = sampler.add_noise(latents, sampler.timesteps[0])
+
+            to_idle(encoder)
+        else:
+            # (batch_size, 4, Latents_Height, Latents_Width)
+            latents = torch.randn(latents_shape, generator=generator, device=device)
+
+        diffusion = models["diffusion"]
+        diffusion.to(device)
+
+        timesteps = tqdm(sampler.timesteps)
+        for i, timestep in enumerate(timesteps):
+            # (1, 320)
+            time_embedding = get_time_embedding(timestep).to(device)
+
+            # (batch_size, 4, Latents_Height, Latents_Width)
+            model_input = latents
+
+            if do_cfg:
+                # (2 * batch_size, 4, Latents_Height, Latents_Width)
+                model_input = model_input.repeat(2, 1, 1, 1)
+
+            # model_output is the predicted noise
+            model_output = diffusion(model_input, context, time_embedding)
+
+            if do_cfg:
+                output_cond, output_uncond = model_output.chunk(2)
+                model_output = cfg_scale * (output_cond - output_uncond) + output_uncond
+
+            # (batch_size, 4, Latents_Height, Latents_Width)
+            latents = sampler.step(timestep, latents, model_output)
+
+        to_idle(diffusion)
+        decoder = models["decoder"]
+        decoder.to(device)
+
+        # (batch_size, 4, Latents_Height, Latents_Width) -> (batch_size, 3, Height, Width)
+        images = decoder(latents)
+        to_idle(decoder)
+        images = rescale(images, (-1, 1), (0, 255), clamp=True)
+
+        # (batch_size, Channel, Height, Width) -> (batch_size, Height, Width, Channel)
+        images = images.permute(0, 2, 3, 1)
+        images = images.to("cpu", torch.uint8).numpy()
+        return images[0]
+    
+def rescale(x, old_range, new_range, clamp=False):
+    old_min, old_max = old_range
+    new_min, new_max = new_range
+    x -= old_min
+    x *= (new_max - new_min) / (old_max - old_min)
+    x += new_min
+    if clamp:
+        x = x.clamp(new_min, new_max)
+    return x
+
+def get_time_embedding(timestep):
+    # (160,)
+    freqs = torch.pow(10000, -torch.arange(start=0, end=160, dtype=torch.float32) / 160) 
+    # (1, 160)
+    x = torch.tensor([timestep], dtype=torch.float32)[:, None] * freqs[None]
+    # (1, 320)
+    return torch.cat([torch.cos(x), torch.sin(x)], dim=-1)
