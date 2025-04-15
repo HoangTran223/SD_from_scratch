ad400305676cdbbe4fc7de584715ad4c05104be1
HoangTran223 <tranvuonghoang204@gmail.com>
2025-04-15 16:16:33 +0700

    Update

--- sd/encoder.py
+++ sd/encoder.py
@@ -6,8 +6,8 @@ from .decoder import VAE_AttentionBlock, VAE_ResidualBlock
 
 class VAE_Encoder(nn.Module):
     def __init__(self):
-        super().__init__()
-         self.layers = nn.ModuleList([
+        super(VAE_Encoder, self).__init__()
+        self.layers = nn.ModuleList([
             # (Batch_size, Channel, Height, Width) -> (Batch_size, 128, Height, Width)
             nn.Conv2d(3, 128, kernel_size=3, padding=1),
 
