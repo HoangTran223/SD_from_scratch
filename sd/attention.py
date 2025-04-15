37fc859929e5a243e543f646c152d769312947d3
HoangTran223 <tranvuonghoang204@gmail.com>
2025-04-15 15:29:03 +0700

    Update

--- sd/attention.py
+++ sd/attention.py
@@ -55,12 +55,12 @@ class SelfAttention(nn.Module):
 
 
 class CrossAttention(nn.Module):
-    def __init__(self, n_heads: int, d_embed: int, d_cross: int, int_proj_bias=True, out_proj_bias=True):
+    def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias=True, out_proj_bias=True):
         super().__init__()
 
-        self.q_proj = nn.Linear(d_embed, d_embed, bias=int_proj_bias)
-        self.k_proj = nn.Linear(d_cross, d_embed, bias=int_proj_bias)
-        self.v_proj = nn.Linear(d_cross, d_embed, bias=int_proj_bias)
+        self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
         self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
 
         self.n_heads = n_heads
